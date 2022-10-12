81133e81c28e30bdb6201237990ce1b325f4284c
Vladimir <kuznetsov.vi03@mail.ru>
2022-10-12 20:58:07 +0300

    task2

---
+++ task2.py
@@ -0,0 +1,9 @@
+# 2. Напишите программу для проверки истинности утверждения not (X or Y or Z) = not X and not Y and not Z для всех значений предикат.
+
+print('Задание 2')
+
+for x in range(2):
+        for y in range(2):
+            for z in range(2):
+                print(not (x or y or z) == (not x and not y and not z))
+                print(x, y, z)
